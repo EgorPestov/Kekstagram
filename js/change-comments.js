9dbcc14b2811a8df1757c253c38da31c6d0761ca
EgorPestov <egorpestov2012@yandex.ru>
2023-04-24 10:10:12 +0400

    fix.

--- js/change-comments.js
+++ js/change-comments.js
@@ -11,13 +11,13 @@ const onLoaderClick = () => {
   let hiddenCommentsLength = commentsContainer.querySelectorAll('.hidden').length;
 
   if (hiddenCommentsLength > SHOWABLE_COMMENTS_COUNT) {
-    for (let i = 0; i <= commentsLength - hiddenCommentsLength - 1 + SHOWABLE_COMMENTS_COUNT; i++) {
+    for (let i = 0; i < commentsLength - hiddenCommentsLength + SHOWABLE_COMMENTS_COUNT; i++) {
       commentsContainer.children[i].classList.remove('hidden');
     }
     hiddenCommentsLength = commentsContainer.querySelectorAll('.hidden').length;
     visibleCommentsCount.textContent = commentsLength - hiddenCommentsLength;
   } else {
-    for (let i = 0; i <= commentsLength - 1; i++) {
+    for (let i = 0; i < commentsLength; i++) {
       commentsContainer.children[i].classList.remove('hidden');
     }
     commentsLoader.classList.add('hidden');
